f6bff12ddb4aa9c9d9e132334847c41b913e4f98
Fernando Bueno Gómez <fbueno@arsys.es>
2021-09-08 17:27:59 +0200

    Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+  subscription$ : Subscription | null = null;
+
+  ngOnInit() : void {
+    const observable$ : Observable<any> = new Observable( (observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    this.subscription$ = observable$.subscribe(
+      ( value: any)  => console.log(value),
+      ( err: any) => {},
+      () => console.info( "completed!" )
+    );
+  }
+
+  ngOnDestroy() : void {
+    this.subscription$?.unsubscribe();
+  }
 }
