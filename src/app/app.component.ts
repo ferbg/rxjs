0e839c6157cc8cf3ff01aeb39733ad685dfebef6
Fernando Bueno Gómez <fbueno@arsys.es>
2021-09-22 14:39:10 +0200

    Ejercicio obligatorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +12,34 @@ import { debounceTime } from 'rxjs/operators';
 export class AppComponent {
   searchString : string = "";
   searchSubject$: Subject<string> = new Subject<string>();
-  subscription$: Subscription | null = null;
+  results$ : any;
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() : void {
-    this.subscription$ = this.searchSubject$.pipe(debounceTime(1000)).subscribe( x => console.log('buscando... ', x))
+    this.results$ = this.searchSubject$.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap( searchString => this.queryAPI(searchString) )
+    );
+  }
+
+  queryAPI( searchString: string ){
+    console.log('calling reddit...', searchString);
+    return this.http.get(`https://www.reddit.com/r/aww/search.json?q=${searchString}`).pipe(
+      tap( (result: any) => console.log('...results:', result) ),
+      map( (result: any) => result.data.children.filter( (item: any) => item.data.thumbnail !== 'default' && item.data.thumbnail !== 'self')),
+      tap( (result: any) => console.log('results after map (&filter)', result) ),
+    );
   }
 
   inputChanged($event: any) {
-    console.info( "entrada teclado: ", $event );
+    console.info( "$event: ", $event );
     this.searchSubject$.next($event);
   }
 
   ngOnDestroy() : void {
-    this.subscription$?.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
